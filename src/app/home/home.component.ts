e083e390ceeaa979965033933c4654ea991fd6b1
Ujjawal Solanki <ujjawal.solanki@hotwaxsystems.com>
2018-05-02 10:54:41 +0530

    Improved: Added filter logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SCHEDULE} from '../providers/mock-schedule';
+import {TEAMS} from "../providers/mock-teams";
+import {VENUES} from "../providers/mock-venue";
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,92 @@ export class HomeComponent implements OnInit {
 
   // schedule list
   schedules: any = SCHEDULE;
+  teams: any = TEAMS;
+  venues: any = VENUES;
+  selectedVenue: any = {};
+  selectedTeam: any = {};
+  fixtureList: Array<any> = [];
+  filteredFixtureList: any = [];
 
-  constructor() { }
+  constructor() {
+    this.fixtureList = this.schedules;
+    this.filteredFixtureList = this.schedules;
+    this.selectedTeam = this.teams[0];
+    this.selectedVenue = this.venues[0];
+  }
 
   ngOnInit() {
+  }
+
+  onTeamSelect() {
+    if (this.selectedTeam.name !== 'All Teams') {
+      if (this.selectedVenue.name !== 'All Venues') {
+        if (this.filteredFixtureList.length) {
+          this.filteredFixtureList = this.filteredFixtureList.filter(
+            fixture => fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name
+          );
+        } else {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => (fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name) &&
+              (fixture.city === this.selectedVenue.name)
+          );
+        }
+      } else {
+        if (this.filteredFixtureList.length) {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name
+          );
+        } else {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => (fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name) &&
+              (fixture.city === this.selectedVenue.name)
+          );
+        }
+      }
+    } else {
+      if (this.selectedVenue.name !== 'All Venues'){
+        this.filteredFixtureList = this.fixtureList.filter(
+          fixture => fixture.city === this.selectedVenue.name
+        );
+      } else {
+        this.filteredFixtureList = this.fixtureList;
+      }
+    }
+  }
+  onVenueSelect() {
+    if (this.selectedVenue.name !== 'All Venues') {
+      if (this.selectedTeam.name !== 'All Teams') {
+        if (this.filteredFixtureList.length) {
+          this.filteredFixtureList = this.filteredFixtureList.filter(
+            fixture => fixture.city === this.selectedVenue.name
+          );
+        } else {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => (fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name)
+              && (fixture.city === this.selectedVenue.name)
+          );
+        }
+      } else {
+        if (this.filteredFixtureList.length) {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => fixture.city === this.selectedVenue.name
+          );
+        } else {
+          this.filteredFixtureList = this.fixtureList.filter(
+            fixture => (fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name)
+              && (fixture.city === this.selectedVenue.name)
+          );
+        }
+      }
+    } else {
+      if (this.selectedTeam.name !== 'All Teams') {
+        this.filteredFixtureList = this.fixtureList.filter(
+          fixture => fixture.team1 === this.selectedTeam.name || fixture.team2 === this.selectedTeam.name
+        );
+      } else {
+        this.filteredFixtureList = this.fixtureList;
+      }
+    }
   }
 
 }
