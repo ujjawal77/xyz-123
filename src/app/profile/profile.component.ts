ff0527faf7e84a3b352816de0ca11369e839e522
Nitish <nitish.mishra@hotwaxsystems.com>
2018-05-13 10:34:44 +0530

    Improved : UI of profile screen

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChallengeService } from '../providers/challenge.service';
+import {SCHEDULE} from "../providers/mock-schedule";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  schedules: any = SCHEDULE;
+  profile: any;
+  users: any;
 
-  constructor() { }
+  constructor(private challengeService: ChallengeService) { }
 
   ngOnInit() {
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+    this.challengeService.getUsers().subscribe((result: any) => {
+      this.users = result;
+      console.log(result);
+    });
   }
 
 }
